3b21f76e79646e5374f02a3f4e7051797564f248
Jokuurinio <162377306+Jokuurinio@users.noreply.github.com>
2024-09-15 12:30:27 +0200

    ToDo-Form and ToDo-List added

---
+++ Frontend/src/components/ToDoForm.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import '../styles/todo.css';
+
+
+function ToDoForm () {
+  const [entries, setEntries] = useState({});
+  const store = (e) => {
+    setEntries({
+      ...entries,
+      //...Spread Operator übernimmt entries
+      // ergänzt Attribut – Wert Paar aus input
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+  
+    const submitData = {
+      content: {
+        title: entries.title,
+        description: entries.description,
+        dueDate: entries.dueDate,
+        completed: "false",
+      },
+    };
+  
+    console.log('Submitting Data:', submitData); 
+
+    fetch("http://localhost:8080/tasks/documents", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(submitData),
+    })
+      .then((response) => response.json()) 
+      .then(() => {
+        setEntries({}); 
+      });
+  };
+  
+  return (
+    <div className='InputFormular'>
+    <form onSubmit={submit}>
+      <div>
+        <label>Title:</label>
+        <input 
+          type="text" 
+          placeholder="Task title" 
+          name = "title"
+          onChange={store} 
+          required 
+        />
+      </div>
+      <div>
+        <label>Description:</label>
+        <textarea 
+          placeholder="Task description" 
+          name = "description"
+          onChange={store} 
+          required 
+        />
+      </div>
+      <div>
+        <label>Due Date:</label>
+        <input 
+          type="date" 
+          name = "dueDate"
+          onChange={store} 
+          required 
+        />
+      </div>
+      <button type="submit">Add Task</button>
+    </form>
+    </div>
+  );
+};
+
+export default ToDoForm;
---
+++ Frontend/src/components/ToDoList.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+export default function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+
+  //Funktion um eine Anfrage an die REST zu senden und die Rückgabe in einen Array tasks zu speichern.
+  const fun = function () {
+    console.log("useEffect wurde aufgerufen");
+    fetch("http://localhost:8080/tasks/documents") // GET Request auf REST API
+      .then((response) => response.json()) //konvertiere Response String zu JSON Objekt
+      .then((tasks) => setTasks(tasks));
+  };
+
+  // Wird immer ausgeführt wenn die ToDoList Komponente geladen wird.
+  useEffect(fun, []);
+
+  return (
+    <>
+      <table id="listig">
+        <tbody>
+          {tasks.map((q) => (
+            <tr>
+              <td>{q.id}</td>
+              <td>{q.title}</td>
+              <td>{q.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
